ade954da737b0c481acbfda6896ee25718174424
yrnana <nyryn0945@gmail.com>
2024-07-12 21:15:34 +0900

    chore: vite 기본 실행파일 추가 & 안쓰는 파일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-// import './App.css';
 import { Container, Box, Typography, CssBaseline } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Stopwatch from './components/Stopwatch';
 import ExerciseList from './components/ExerciseList';
 
-const theme = createTheme()
+const theme = createTheme();
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme = {theme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Container maxWidth="sm">
-        <Box sx={{ my: 4}}>
+        <Box sx={{ my: 4 }}>
           <Typography variant="h4" component="h1" gutterBottom>
             운동 트래커
           </Typography>
@@ -24,6 +22,6 @@ const App: React.FC = () => {
       </Container>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    open: true,
+    proxy: {
+      // 개발모드에서 /api로 시작하는 호출은 8080에서 프록시 호출
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: 'build',
+  },
+});
